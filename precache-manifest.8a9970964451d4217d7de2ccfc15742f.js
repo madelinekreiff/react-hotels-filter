78d72667366f414dbc8a41e260e9ebd2d2ff6043
Madeline Reiff <81939293+madelinekreiff@users.noreply.github.com>
2021-09-11 20:54:34 -0700

    Changed file paths from csb-mgrh1 to new repo name

--- precache-manifest.8a9970964451d4217d7de2ccfc15742f.js
+++ precache-manifest.8a9970964451d4217d7de2ccfc15742f.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "e9b7570748cc314a13da",
-    "url": "https://.github.io/csb-mgrh1/static/css/main.892ef6da.chunk.css"
+    "url": "https://madelinekreiff.github.io/react-hotels-filter/static/css/main.892ef6da.chunk.css"
   },
   {
     "revision": "e9b7570748cc314a13da",
-    "url": "https://.github.io/csb-mgrh1/static/js/main.352436a4.chunk.js"
+    "url": "https://madelinekreiff.github.io/react-hotels-filter/static/js/main.352436a4.chunk.js"
   },
   {
     "revision": "04261ba3066b483949e9",
-    "url": "https://.github.io/csb-mgrh1/static/js/runtime~main.9147123c.js"
+    "url": "https://madelinekreiff.github.io/react-hotels-filter/static/js/runtime~main.9147123c.js"
   },
   {
     "revision": "bd70679013187c326ad9",
-    "url": "https://.github.io/csb-mgrh1/static/js/2.25f0cea1.chunk.js"
+    "url": "https://madelinekreiff.github.io/react-hotels-filter/static/js/2.25f0cea1.chunk.js"
   },
   {
     "revision": "fec9a37e204dd4e0c697a91e9a091616",
-    "url": "https://.github.io/csb-mgrh1/index.html"
+    "url": "https://madelinekreiff.github.io/react-hotels-filter/index.html"
   }
 ];
